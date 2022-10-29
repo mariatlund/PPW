5d8943a11da8037ed7c2a1baa7d84027f66b863c
Maria Lundqvist <mari28rd@stud.kea.dk>
2022-10-29 17:44:39 +0200

    project cards gave me a minor heart attack, i need a break

---
+++ script/script.js
@@ -0,0 +1,12 @@
+// "use strict";
+// import { animate, scroll } from "https://cdn.skypack.dev/motion";
+// window.addEventListener("DOMContentLoaded", load);
+// function load() {
+//   document.querySelectorAll("article").forEach((item) => {
+//     scroll(animate(item, { opacity: [0, 1, 1] }), {
+//       target: item,
+//       duration: 1,
+//       offset: ["start end", "end start"],
+//     });
+//   });
+// }
