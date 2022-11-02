39d8b4ccd11bfb778b0df5540fb9aefa5155b155
Maria Lundqvist <mari28rd@stud.kea.dk>
2022-11-02 18:11:34 +0100

    adding scroll effect, removing some unused code

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
-// "use strict";
-// import { animate, scroll } from "https://cdn.skypack.dev/motion";
-// window.addEventListener("DOMContentLoaded", load);
-// function load() {
-//   document.querySelectorAll("article").forEach((item) => {
-//     scroll(animate(item, { opacity: [0, 1, 1] }), {
-//       target: item,
-//       duration: 1,
-//       offset: ["start end", "end start"],
-//     });
-//   });
-// }
+"use strict";
+import { animate, scroll } from "https://cdn.skypack.dev/motion";
+window.addEventListener("DOMContentLoaded", load);
+function load() {
+  document.querySelectorAll("h2").forEach((item) => {
+    scroll(animate(item, { color: "#3153ff" }), {
+      target: item,
+      duration: 1,
+      offset: ["start end", "end start"],
+    });
+  });
+}
